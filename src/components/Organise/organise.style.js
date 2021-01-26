fefb6ee0fd222f76a96df26713b8393402934193
Moses ubah <ubahmohzys@gmail.com>
2021-01-26 14:39:45 +0000

    mobile design formatting

--- src/components/Organise/organise.style.js
+++ src/components/Organise/organise.style.js
@@ -9,6 +9,15 @@ export const Container = styled.div`
   .lower-card {
     width: 50%;
     margin: 0 auto;
+
+    @media (max-width: 575px) {
+      
+    }
+  }
+  @media (max-width: 575px) {
+    .text-box {
+      width: 100%;
+    }
   }
 `;
 
@@ -19,26 +28,56 @@ export const Text = styled.h1`
   font-size: 3.375rem;
   letter-spacing: -0.03125rem;
   font-weight: 300;
+
+  @media (max-width: 575px) {
+    width: 80%;
+    padding: 5rem 0;
+    font-size: 2.5rem;
+    font-weight: 300;
+    line-height: 1;
+    letter-spacing: -0.03125rem;
+  }
 `;
 
 export const Section = styled.div`
   width: 75%;
   margin: 0 auto;
 
+  @media (max-width: 575px) {
+    width: 80%;
+    align-items: center;
+  }
+
   .card {
     display: flex;
     align-items: center;
     padding-bottom: 7.5rem;
 
+    @media (max-width: 575px) {
+      flex-wrap: wrap;
+    }
+
     img {
       width: 50%;
       height: auto;
+
+      @media (max-width: 575px) {
+        display: block;
+        width: 100%;
+        height: auto;
+        margin-bottom: 2.5rem;
+      }
     }
 
     .card-text {
       width: 50%;
       padding-left: calc(8.33333% + 1.875rem);
       text-align: left;
+
+      @media (max-width: 575px) {
+        width: 100%;
+        padding: 0;
+      }
     }
   }
 `;
@@ -48,6 +87,12 @@ export const Heading = styled.h2`
   font-weight: 300;
   line-height: 1.1;
   margin-bottom: 2rem;
+
+  @media (max-width: 575px) {
+    font-size: 1.75rem;
+    font-weight: 400;
+    line-height: 1.1;
+  }
 `;
 
 export const SmallText = styled.p`
